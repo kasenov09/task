d0cb631634aab7332e840b077fc0f0932491e8e0
kasenov09 <kasenov09>
2023-08-08 18:08:39 +0600

    sad

---
+++ .history/hp/mio/models_20230808180652.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Course(models.Model):
+    name = models.CharField(max_length= 100)
+    description = models.TextField()
+    students = models.ManyToManyField(Student)
